7ad89ff3b18f2e65cdcea12c181915f069fef139
annas <annasan14052003@gmail.com>
2024-04-07 17:21:43 +0300

    added mapstruct test

--- education/src/test/java/education/education/EducationApplicationTests.java
+++ education/src/test/java/education/education/EducationApplicationTests.java
@@ -1,13 +1,35 @@
 package education.education;
 
+import education.education.dtos.SubjectDTO;
+import education.education.mappers.SubjectMapper;
+import education.education.models.Subject;
+import education.education.repositories.SubjectRepository;
+import education.education.services.SubjectService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EducationApplicationTests {
+    @Autowired
+    private SubjectService subjectService;
+    @Autowired
+    private SubjectMapper subjectMapper;
+    @Autowired
+    private SubjectRepository subjectRepository;
 
     @Test
-    void contextLoads() {
-    }
+    @DisplayName("Map subject to dto")
+    void test() {
+        Subject subject = subjectRepository.findById(2).get();
+        SubjectDTO subjectDTO = subjectMapper.toDTO(subject);
 
+        Assertions.assertNotNull(subject);
+        Assertions.assertNotNull(subjectDTO);
+        Assertions.assertEquals(subjectDTO.getId(), subject.getId());
+        Assertions.assertEquals(subjectDTO.getName(), subject.getName());
+//        Assertions.assertEquals(subjectDTO.getImage(), subject.getImage());
+    }
 }
