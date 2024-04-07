c03047510b13404be2a1f2f154b0b890138e4420
annas <annasan14052003@gmail.com>
2024-04-07 17:20:39 +0300

    added repositories

--- education/src/main/java/education/education/repositories/ControlWorkRepository.java
+++ education/src/main/java/education/education/repositories/ControlWorkRepository.java
@@ -3,5 +3,8 @@ package education.education.repositories;
 import education.education.models.ControlWork;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ControlWorkRepository extends JpaRepository<ControlWork, Integer> {
+    Optional<ControlWork> findById(int id);
 }
