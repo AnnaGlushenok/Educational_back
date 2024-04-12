23b74ebdbcde68159ab1534aa277dd813aa1ebde
annas <annasan14052003@gmail.com>
2024-04-12 15:36:20 +0300

    clear model

--- education/src/main/java/education/education/models/Unit.java
+++ education/src/main/java/education/education/models/Unit.java
@@ -20,24 +20,20 @@ public class Unit {
     private Integer id;
     private String name;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-//    @OnDelete(action = OnDeleteAction.CASCADE)
+    @ManyToOne(fetch = FetchType.EAGER)
     private Subject subject;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "class_id")
-//    @OnDelete(action = OnDeleteAction.CASCADE)
-    private Class aClass;
+    private Class classEntity;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-//    @OnDelete(action = OnDeleteAction.CASCADE)
+    @ManyToOne(fetch = FetchType.EAGER)
     private Test test;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-//    @OnDelete(action = OnDeleteAction.CASCADE)
+    @ManyToOne(fetch = FetchType.EAGER)
     private ControlWork controlWork;
 
-    @ManyToMany(fetch = FetchType.LAZY, targetEntity = Paragraph.class)
+    @ManyToMany(fetch = FetchType.EAGER, targetEntity = Paragraph.class)
     @JoinTable(
             name = "units_paragraphs",
             joinColumns = @JoinColumn(name = "id_unit"),
