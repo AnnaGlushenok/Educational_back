205beecc35b6365af020cff3e7a750871a897058
annas <annasan14052003@gmail.com>
2024-04-12 15:41:29 +0300

    added unit repo

--- education/src/main/java/education/education/repositories/UnitRepository.java
+++ education/src/main/java/education/education/repositories/UnitRepository.java
@@ -3,8 +3,11 @@ package education.education.repositories;
 import education.education.models.Unit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UnitRepository extends JpaRepository<Unit, Integer> {
     Optional<Unit> findById(int id);
+
+    List<Unit> findAllBySubjectIdAndClassEntityId(int subjectId, int classId);
 }
