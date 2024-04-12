a309311db5d2fb583572bfdf43fe3c9f635e2a9d
annas <annasan14052003@gmail.com>
2024-04-12 15:41:50 +0300

    added class provider

---
+++ education/src/main/java/education/education/services/interfaces/ClassProvider.java
@@ -0,0 +1,7 @@
+package education.education.services.interfaces;
+
+import education.education.dtos.ClassDTO;
+
+public interface ClassProvider extends DataProvider<ClassDTO> {
+//    List<ClassDTO> findBySubjectName(String name);
+}
