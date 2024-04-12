f2378540e82cc35f70838cc4bf34354ee2cfb125
annas <annasan14052003@gmail.com>
2024-04-12 15:41:17 +0300

    added controller

---
+++ education/src/main/java/education/education/controllres/LessonController.java
@@ -0,0 +1,13 @@
+package education.education.controllres;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("lesson")
+public class LessonController {
+//    @GetMapping("get/{unitId}")
+//    public getAllByUnitId(@PathVariable int unitId) {
+//
+//    }
+}
