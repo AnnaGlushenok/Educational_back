c45d07bd73227c6c12c918f3dbff1cfde3fe1446
annas <annasan14052003@gmail.com>
2024-04-21 14:52:08 +0300

    added testController

---
+++ education/src/main/java/education/education/controllres/TestController.java
@@ -0,0 +1,28 @@
+package education.education.controllres;
+
+import education.education.dtos.TestDTO;
+import education.education.services.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+    @Autowired
+    private TestService testService;
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<?> getById(@PathVariable int id) {
+        try {
+            TestDTO testDTO = testService.findById(id);
+            return new ResponseEntity<>(testDTO, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+}
