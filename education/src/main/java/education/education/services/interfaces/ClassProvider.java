17069cebebed091b7631f63f814398659e359601
annas <annasan14052003@gmail.com>
2024-04-28 16:14:38 +0300

    removed unused classes

--- education/src/main/java/education/education/services/interfaces/ClassProvider.java
+++
@@ -1,7 +0,0 @@
-package education.education.services.interfaces;
-
-import education.education.dtos.ClassDTO;
-
-public interface ClassProvider extends DataProvider<ClassDTO> {
-//    List<ClassDTO> findBySubjectName(String name);
-}
