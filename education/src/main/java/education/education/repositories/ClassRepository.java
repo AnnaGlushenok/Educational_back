fd7f82465499c1ec7f613c4f22ef2aeb891b85d5
annas <annasan14052003@gmail.com>
2024-04-28 19:40:08 +0300

    refactor

--- education/src/main/java/education/education/repositories/ClassRepository.java
+++ education/src/main/java/education/education/repositories/ClassRepository.java
@@ -3,8 +3,6 @@ package education.education.repositories;
 import education.education.models.Class;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ClassRepository extends JpaRepository<Class, Integer> {
-    Optional<Class> findById(int id);
+
 }
--- education/src/main/java/education/education/repositories/SubjectRepository.java
+++ education/src/main/java/education/education/repositories/SubjectRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Integer> {
-    Optional<Subject> findById(int id);
+
 }
