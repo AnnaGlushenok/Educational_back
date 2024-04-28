56a8f14624a6b2f4941e3d66d1b3bda3ea05b425
annas <annasan14052003@gmail.com>
2024-04-28 17:34:48 +0300

    refactor

--- education/src/main/java/education/education/EducationApplication.java
+++ education/src/main/java/education/education/EducationApplication.java
@@ -3,32 +3,12 @@ package education.education;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @EnableDiscoveryClient
 public class EducationApplication {
     public static void main(String[] args) {
         SpringApplication.run(EducationApplication.class, args);
-    }
-
-    @GetMapping("/ed")
-    public String ed() {
-        return "EDUCATION SERVICE";
-    }
-
-    @GetMapping("mod2/mm")
-    public String sad() {
-        return "/mod2/get";
-    }
-
-    @PostMapping("/add")
-    public String te(@RequestBody String name) {
-        return name + " mod!!!";
     }
 }
 
