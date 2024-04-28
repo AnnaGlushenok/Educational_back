fb6d58e6e7f9c0036ab7570da3b6fdd8e635a6a4
annas <annasan14052003@gmail.com>
2024-04-28 16:03:11 +0300

    removed unused methods

--- education/src/main/java/education/education/services/interfaces/DataProvider.java
+++ education/src/main/java/education/education/services/interfaces/DataProvider.java
@@ -1,27 +1,8 @@
 package education.education.services.interfaces;
 
-import java.util.List;
-
 /**
- * The DataProvider interface provides methods for accessing data of a specific type T.
- * Classes implementing this interface provide functionality to retrieve a list of all objects of type T
- * and to find an object of type T by its identifier.
- *
- * @param <T> the type of objects that the DataProvider works with
+ * The DataProvider interface.
  */
 public interface DataProvider<T> {
-    /**
-     * Retrieves a list of all objects of type T.
-     *
-     * @return a list of all objects of type T
-     */
-    List<T> findAll();
 
-    /**
-     * Finds an object of type T by its identifier.
-     *
-     * @param id the identifier of the object of type T
-     * @return the found object of type T, or null if the object is not found
-     */
-    T findById(int id);
 }
